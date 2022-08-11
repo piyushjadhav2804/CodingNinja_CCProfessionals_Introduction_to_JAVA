61ae9ca219f9dbbac31787340570a7c44cfa4c49
piyushjadhav2804 <71430828+piyushjadhav2804@users.noreply.github.com>
2022-08-12 03:08:27 +0530

    Create checkAP.java

---
+++ Test1/checkAP.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		// Write your code here
+        
+		Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int diff= b - a;
+        
+        boolean flag = true;
+        
+        for(int i=2; i<n; i++) {
+            
+            int c = sc.nextInt();
+            
+            if(c - b != diff) {
+                flag = false;
+                break;
+            }
+            
+            b = c;
+        }
+        
+        System.out.println(flag);
+
+	}
+}
