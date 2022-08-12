20aa4aea7bd11dfe730b3dcf4decfc4c7a738056
piyushjadhav2804 <71430828+piyushjadhav2804@users.noreply.github.com>
2022-08-12 11:53:46 +0530

    Create sumArray.java

---
+++ Arrays/sumArray.java
@@ -0,0 +1,16 @@
+
+public class Solution {
+	
+	public static int sum(int[] arr) {
+		//Your code goes here
+        
+        int ans=0;
+        
+        for(int i=0; i<arr.length; i++) {
+            
+            ans += arr[i];
+        }
+        
+        return ans;
+	}
+}
