67585fe9b850c8ee0ee1a695bf15418044b9e357
piyushjadhav2804 <71430828+piyushjadhav2804@users.noreply.github.com>
2022-08-17 06:01:48 +0530

    Create palindrome.java

---
+++ Strings/palindrome.java
@@ -0,0 +1,24 @@
+
+public class Solution {
+
+	public static boolean isPalindrome(String str) {
+		//Your code goes here
+        
+        if(str.length() <= 1)
+            return true;
+        
+        int left=0, right=str.length()-1;
+        
+        while(left < right) {
+            
+            if(str.charAt(left) != str.charAt(right))
+                return false;
+            
+            left++;
+            right--;
+        }
+        
+        return true;
+	}
+
+}
