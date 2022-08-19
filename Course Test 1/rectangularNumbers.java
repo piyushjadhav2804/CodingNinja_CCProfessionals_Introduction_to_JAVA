d0f78d91a207692921f5cc09fb779e53d429699a
piyushjadhav2804 <71430828+piyushjadhav2804@users.noreply.github.com>
2022-08-19 13:47:05 +0530

    Create rectangularNumbers.java

---
+++ Course Test 1/rectangularNumbers.java
@@ -0,0 +1,26 @@
+public class solution {
+
+	public static void print(int n) {
+	//Write your code here
+        
+        int i, j, a;
+        for (i = -(n - 1); i < n; i++) {
+            a = n;
+            for (j = -(n - 1); j < n; j++) {
+                if (Math.abs(i) < Math.abs(j)) {
+                    if (j < 0) {
+                        System.out.print(a);
+                        a--;
+                    } else {
+                        a++;
+                        System.out.print(a);
+                    }
+                } else {
+                    System.out.print(a);
+                }
+            }
+            System.out.println();
+        }
+	}
+
+}
